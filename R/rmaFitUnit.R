7e6cf2d215bff0db74e44c41003d2ea4699f2304
Mark Robinson <mrobinson@wehi.edu.au>
2008-12-08 10:52:38 +0000

    change to correct FLM
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flagme@35714 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/rmaFitUnit.R
+++ R/rmaFitUnit.R
@@ -1,18 +1,58 @@
-.rmaFitPeak <- function(u,PsiCode=0,PsiK=1.345) {
+.rmaFitPeak <- function(u,maxit=5,mzEffect=TRUE,cls=NULL,fitSample=TRUE,fitOrCoef=c("coef","fit"),TRANSFORM=log2) {
+  require(MASS)
   d<-u$data
-  d<-d[rowSums(is.na(d) | d==0)==0,]
-  y<-log(d,base=2)
-  beta<-rep(NA,ncol(y))
-  keep<-colSums(is.na(y))==0
-  y<-y[,keep]
-  fit<-.Call("R_rlm_rma_default_model",y,PsiCode,PsiK,PACKAGE="preprocessCore")
-  J <- ncol(y)
-  est <- fit$Estimates
-  #se <- fit$StdErrors
-  beta.tmp <- est[1:J]
-  alpha <- est[(J + 1):length(est)]
-  alpha[length(alpha)] <- -sum(alpha[1:(length(alpha)-1)])
-  beta[keep]<-beta.tmp
-  list(alpha=alpha,beta=beta,res=fit$Residuals,keep=keep)
+  fitOrCoef=match.arg(fitOrCoef)
+  k<-rowSums(d==0 | is.na(d))==0  # cannot have rows with 0s and then take logs
+  #if( any(is.na(d)) ) {
+  #  cat('Unit has NAs.  Please check\n')
+  #  return(NULL)
+  #}
+  y<-as.vector(TRANSFORM(d[k,]))
+  probe<-factor(rep(u$mz[k],ncol(d)))
+  sample<-factor(rep(names(u$rt),each=nrow(d[k,])))
+  clsv<-factor(rep(cls,each=nrow(d[k,])))
+  if( !mzEffect ) {
+    if (fitSample)
+      v<-model.matrix(~-1+sample+probe,contrasts=list(probe=contr.sum))
+    else
+      v<-model.matrix(~-1+clsv+probe,contrasts=list(probe=contr.sum))
+  } else {
+    if ( is.null(cls) )
+      stop("'cls' needs to be specified in order to run this model.")
+    mz<-rep(u$mz[k],ncol(d))
+    if (fitSample) {
+          vv<-model.matrix(~-1+sample+probe,contrasts=list(probe=contr.sum))
+          vv1<-model.matrix(~-1+mz:clsv,contrast=list(clsv=contr.sum))
+          dd<- vv1 %*% attr(vv1,"contrasts")$clsv
+          v<-cbind(vv,dd)
+        } else {
+          vv<-model.matrix(~-1+clsv+probe,contrasts=list(probe=contr.sum))
+          vv1<-model.matrix(~-1+mz:clsv,contrast=list(clsv=contr.sum))
+          dd<- vv1 %*% attr(vv1,"contrasts")$clsv
+          v<-cbind(vv,dd)
+        }
+    #else
+    #  v<-model.matrix(~-1+clsv+probe+mz:clsv,contrasts=list(probe=contr.sum,clsv=contr.sum))
+    #v<-v[,-ncol(v)]  # make design matrix full rank
+  }
+  fit<-rlm(y~-1+v,maxit=maxit)
+  cat(".")
+  if (fitOrCoef=="coef") {
+    sample<-fit$coef[1:ncol(d)]
+        fragment<-fit$coef[(ncol(d)+1):(ncol(d)+nrow(d[k,])-1)]
+        fragment<-c(fragment,-sum(fragment))
+        if(mzEffect) {
+          mzeff<-c(fit$coef[(ncol(d)+nrow(d[k,])):length(fit$coef)],0)*mean(u$mz[k])
+          sample<-sample+mzeff[sort(factor(cls))]
+        }
+        list(sample=sample,fragment=fragment)
+  } else {
+    # save a little space for stuff that probably don't need
+    #fit$qr<-NULL
+        #fit$model<-NULL
+        #fit$weights<-NULL
+        #fit$effects<-NULL
+        #fit$fitted.values<-NULL
+    list(fit=fit,v=v,y=y)
+  }
 }
-
