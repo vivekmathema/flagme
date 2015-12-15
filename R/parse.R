3e26f5b4421a746e3c2a25536461192f34334b17
Riccardo Romoli <riccardo.romoli@unifi.it>
2015-12-15 10:10:59 +0000

    fixed mzrange bug in peaksDataset(), parseELU() and peaksAlignments() functions
    From: Riccardo <r@mbp>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flagme@111488 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/parse.R
+++ R/parse.R
@@ -1,67 +1,72 @@
 
-parseELU<-function(f,min.pc=.01,mz=seq(50,550),rt.cut=.008,rtrange=NULL) {
-  mostart<-function(u,key="MO") { which(substr(u,1,2)==key) }
-  v<-read.table(f,comment.char="",sep="\n",stringsAsFactors=FALSE)
-  keep<-substr(v[,1],1,5)=="NAME:"
-  hdr<-v[keep,]  # header lines
-  hdr<-strsplit(hdr,"\\|")
-  rtis<-unlist(lapply(hdr,FUN=mostart,key="RT"))
-  rts<-rep(NA,length(rtis))
-  for(i in 1:length(rts))
-    rts[i]<-hdr[[i]][rtis[i]]
-  rts<-as.numeric(gsub("RT","",rts))
-  
-  starts<-which(substr(v[,1],1,9)=="NUM PEAKS")+1
-  ends<-c(which(substr(v[,1],1,5)=="NAME:")[-1]-1,length(v[,1]))
-  if(length(rtrange)==2) {
-    w<-which(rts>=rtrange[1] & rts<=rtrange[2])
-    rts<-rts[w]
-	starts<-starts[w]
-	ends<-ends[w]
-  } else {
-    w<-seq(1,length(starts))
-  }
-  peaks<-matrix(0,nrow=length(mz),ncol=length(starts))
-  for(j in 1:length(starts)) {
-    pks<-strsplit(paste(v[starts[j]:ends[j],1],collapse=""),"\\)\\(")[[1]]
-    pks[1]<-sub("\\(","",pks[1])
-    pks[length(pks)]<-sub("\\)","",pks[length(pks)])
-    aft<-lapply(pks,FUN=function(u,split=" ") .subset2(strsplit(u,split),1)[2])
-	aft<-unlist(lapply(aft,is.na))
-    pks<-lapply(pks,FUN=function(u,split=" ") .subset2(strsplit(u,split),1)[1])
-	pks<-pks[aft]
-    mzc<-as.numeric(unlist(lapply(pks,FUN=function(u,split=",") .subset2(strsplit(u,split),1)[1])))
-    abn<-as.numeric(unlist(lapply(pks,FUN=function(u,split=",") .subset2(strsplit(u,split),1)[2])))
-    mzc<-mzc[abn>min.pc*max(abn)]
-    abn<-abn[abn>min.pc*max(abn)]
-    peaks[match(mzc,mz),j]<-abn
-  }
-  v<-v[keep,][w]
-  v<-strsplit(v,"\\|")
-  rts<-unlist(lapply(v,FUN=mostart,key="RT"))
-  scs<-unlist(lapply(v,FUN=mostart,key="SC"))
-  frs<-unlist(lapply(v,FUN=mostart,key="FR"))
-  ras<-unlist(lapply(v,FUN=mostart,key="RA"))
-  tab<-data.frame(matrix(NA,nrow=length(scs),ncol=5))
-  colnames(tab)<-c("SC","RT","start","end","RA")
-  for(i in 1:nrow(tab)) {
-    tab[i,1]<-as.numeric(sub("SC","",v[[i]][scs[i]]))
-    tab[i,2]<-as.numeric(sub("RT","",v[[i]][rts[i]]))
-	tab[i,3:4]<-as.numeric(strsplit(sub("FR","",v[[i]][frs[i]]),"-")[[1]])
-    tab[i,5]<-as.numeric(sub("RA","",v[[i]][ras[i]]))
-  }
-  #keep<-which(peaks[which(mz==73),]>0 | peaks[which(mz==147),]>0)  # remove peaks that have no evidence of TMS (73,147)
-  #tab<-tab[keep,]
-  #peaks<-peaks[,keep]
-  groups<-cutree(hclust(dist(tab[,2])),h=rt.cut)  # merge peaks that have VERY similiar retention times
-  newpeaks<-matrix(0,nrow=length(mz),ncol=max(groups))
-  newtab<-data.frame(matrix(0,nrow=max(groups),ncol=5)); colnames(newtab)<-colnames(tab)
-  for(i in 1:nrow(newtab)) {
-    newtab[i,]<-apply(tab[groups==i,],2,median)
-	newtab[i,ncol(newtab)]<-sum(tab[groups==i,ncol(newtab)])
-    newpeaks[,i]<-apply(matrix(peaks[,groups==i],nrow=length(mz)),1,sum)
-  }
-  list(peaks=newpeaks,tab=newtab)
+parseELU <- function(f, min.pc=0.01, mz=seq(50, 550), rt.cut=0.008, rtrange=NULL){
+    mostart <- function(u, key = "MO") {
+        which(substr(u, 1, 2) == key)
+    }
+    v <- read.table(f, comment.char="", sep="\n", stringsAsFactors=FALSE)
+    keep <- substr(v[, 1], 1, 5) == "NAME:"
+    hdr <- v[keep, ]
+    hdr <- strsplit(hdr, "\\|")
+    rtis <- unlist(lapply(hdr, FUN=mostart, key="RT"))
+    rts <- rep(NA, length(rtis))
+    for(i in 1:length(rts)) rts[i] <- hdr[[i]][rtis[i]]
+    rts <- as.numeric(gsub("RT", "", rts))
+    starts <- which(substr(v[, 1], 1, 9) == "NUM PEAKS") + 1
+    ends <- c(which(substr(v[, 1], 1, 5) == "NAME:")[-1] - 1, 
+        length(v[, 1]))
+    if(length(rtrange) == 2){
+        w <- which(rts >= rtrange[1] & rts <= rtrange[2])
+        rts <- rts[w]
+        starts <- starts[w]
+        ends <- ends[w]
+    }else{
+        w <- seq(1, length(starts))
+    }
+    peaks <- matrix(0, nrow=length(mz), ncol=length(starts))
+    for(j in 1:length(starts)){
+        pks <- strsplit(paste(v[starts[j]:ends[j], 1], collapse=""), "\\)\\(")[[1]]
+        pks[1] <- sub("\\(", "", pks[1])
+        pks[length(pks)] <- sub("\\)", "", pks[length(pks)])
+        aft <- lapply(pks, function(u, split=" ") .subset2(strsplit(u, split), 1)[2])
+        aft <- unlist(lapply(aft, is.na))
+        pks <- lapply(pks, function(u, split=" ") .subset2(strsplit(u, split), 1)[1])
+        pks <- pks[aft]
+        mzc <- as.numeric(unlist(lapply(pks, function(u, split=",") .subset2(strsplit(u, split), 1)[1])))
+        idx <- which(mzc < range(mz)[1] | mzc > range(mz)[2])# resize mz range 
+        mzc <- mzc[-idx] #
+        abn <- as.numeric(unlist(lapply(pks, function(u, split=",") .subset2(strsplit(u, split), 1)[2])))
+        abn <- abn[-idx] #
+        mzc <- mzc[abn > min.pc * max(abn)]
+        abn <- abn[abn > min.pc * max(abn)]
+        mt <- match(mzc, mz)
+        mt <- mt[!is.na(mt)]
+        peaks[mt, j] <- abn
+    }
+    v <- v[keep, ][w]
+    v <- strsplit(v, "\\|")
+    rts <- unlist(lapply(v, FUN=mostart, key="RT"))
+    scs <- unlist(lapply(v, FUN=mostart, key="SC"))
+    frs <- unlist(lapply(v, FUN=mostart, key="FR"))
+    ras <- unlist(lapply(v, FUN=mostart, key="RA"))
+    tab <- data.frame(matrix(NA, nrow=length(scs), ncol=5))
+    colnames(tab) <- c("SC", "RT", "start", "end", "RA")
+    for(i in 1:nrow(tab)){
+        tab[i, 1] <- as.numeric(sub("SC", "", v[[i]][scs[i]]))
+        tab[i, 2] <- as.numeric(sub("RT", "", v[[i]][rts[i]]))
+        tab[i, 3:4] <- as.numeric(strsplit(sub("FR", "", v[[i]][frs[i]]), "-")[[1]])
+        tab[i, 5] <- as.numeric(sub("RA", "", v[[i]][ras[i]]))
+    }
+    groups <- cutree(hclust(dist(tab[, 2])), h=rt.cut)
+    newpeaks <- matrix(0, nrow=length(mz), ncol=max(groups))
+    newtab <- data.frame(matrix(0, nrow=max(groups), ncol=5))
+    colnames(newtab) <- colnames(tab)
+    for(i in 1:nrow(newtab)){
+        newtab[i, ] <- apply(tab[groups == i, ], 2, median)
+        newtab[i, ncol(newtab)] <- sum(tab[groups == i, ncol(newtab)])
+        newpeaks[, i] <- apply(matrix(peaks[, groups == i], nrow=length(mz)), 
+            1, sum)
+    }
+    list(peaks=newpeaks, tab=newtab)
 }
 
 parseChromaTOF<-function(fn,min.pc=.01,mz=seq(85,500),rt.cut=.008,rtrange=NULL,skip=1, rtDivide=60) {
--- R/peaksAlignment.R
+++ R/peaksAlignment.R
@@ -35,36 +35,44 @@ setMethod("decompress","peaksAlignment",
   })
 
 
-#peaksAlignment<-function(d1,d2,t1,t2,D=1000,timedf=NULL,gap=.5,df=30,w=25,mzind=24,metric=1,transform="none",verbose=TRUE,usePeaks=TRUE,compress=TRUE) {
-peaksAlignment<-function(d1,d2,t1,t2,gap=.5,D=1000,timedf=NULL,df=30,verbose=TRUE,usePeaks=TRUE,compress=TRUE) {
+## peaksAlignment <- function(d1, d2, t1, t2, D=1000, timedf=NULL,
+##                            gap=.5, df=30, w=25, mzind=24, metric=1, transform="none",
+##                            verbose=TRUE, usePeaks=TRUE, compress=TRUE){
+peaksAlignment <- function(d1, d2, t1, t2, gap=.5, D=1000,
+                           timedf=NULL, df=30, verbose=TRUE,
+                           usePeaks=TRUE, compress=TRUE){
 
-  #r<-switch(metric,
-           #normDotProduct(d1,d2,t1,t2,D=D,df=df+abs(ncol(d1)-ncol(d2)),timedf=timedf,verbose=verbose),  # metric=1
-           #cos.ndp.rank(d1,d2,t1,t2,D=D,timedf=timedf,verbose=verbose),                          # metric=2
-           #euclidean(d1,d2,t1,t2,D=D,timedf=timedf,verbose=verbose),                             # metric=3
-           #soai(d1,d2,t1,t2,D=D,timedf=timedf,verbose=verbose),                                  # metric=4
-           #manhattan(d1,d2,t1,t2,D=D,timedf=timedf,verbose=verbose),                             # metric=5
-           #window.metric.c(d1,d2,mzind=mzind,df=df,w=w))                                         # metric=6
-		   #)
-		   
-  r<-normDotProduct(d1,d2,t1,t2,D=D,df=df+abs(ncol(d1)-ncol(d2)),timedf=timedf,verbose=verbose)
-
-  if (verbose)
-    cat("[peaksAlignment] Comparing",ncol(d1),"peaks to",ncol(d2),"peaks -- gap=", gap, "D=", D, "\n")
-  v<-dp(r,gap=gap,verbose=verbose)
-  sim<-0
-  for(i in 1:nrow(v$match)) {
-    sim<-sim+r[v$match[i,1],v$match[i,2]]
-  }
-  sim<-sim/nrow(v$match)
-  if (verbose)
-    cat("[peaksAlignment] ",nrow(v$match),"matched.  Similarity=",sim,"\n")
-  object<-new("peaksAlignment",v=v,r=r,dist=sim,compressed=FALSE,gap=gap,D=D)
-  if(compress) {
-    compress(object)
-  } else {
-    object
-  }
+    ## r <- switch(metric,
+    ##             normDotProduct(d1,d2,t1,t2,D=D,df=df+abs(ncol(d1)-ncol(d2)),timedf=timedf,verbose=verbose),  # metric=1
+    ##             cos.ndp.rank(d1,d2,t1,t2,D=D,timedf=timedf,verbose=verbose),                          # metric=2
+    ##             euclidean(d1,d2,t1,t2,D=D,timedf=timedf,verbose=verbose),                             # metric=3
+    ##             soai(d1,d2,t1,t2,D=D,timedf=timedf,verbose=verbose),                                  # metric=4
+    ##             manhattan(d1,d2,t1,t2,D=D,timedf=timedf,verbose=verbose),                             # metric=5
+    ##             window.metric.c(d1,d2,mzind=mzind,df=df,w=w))                                         # metric=6
+    ##                                                      )
+    r <- normDotProduct(d1, d2, t1, t2, D=D,
+                        df=df + abs(ncol(d1) - ncol(d2)),
+                        timedf=timedf, verbose=verbose)
+    r[is.nan(r)] <- 0 ## remove NaN
+    if(verbose) 
+        cat("[peaksAlignment] Comparing", ncol(d1), "peaks to", 
+            ncol(d2), "peaks -- gap=", gap, "D=", D, "\n")
+    v <- dp(r, gap=gap, verbose=verbose)
+    sim <- 0
+    for(i in 1:nrow(v$match)){
+        sim <- sim + r[v$match[i, 1], v$match[i, 2]]
+    }
+    sim <- sim/nrow(v$match)
+    if(verbose) 
+        cat("[peaksAlignment] ", nrow(v$match), "matched.  Similarity=", 
+            sim, "\n")
+    object <- new("peaksAlignment", v=v, r=r, dist=sim, 
+                  compressed=FALSE, gap=gap, D=D)
+    if(compress){
+        compress(object)
+    }else{
+        object
+    }
 }
 
 
